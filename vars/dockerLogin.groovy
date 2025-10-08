a5395478d688deb6da154a0e61bae48f3b79492d
mr-mayu-mane <mayurmane2577@gmail.com>
2025-10-08 22:38:07 +0530

    node-app docker image

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,5 +2,6 @@ def call(){
 withCredentails([usernamePassword(credentailsId: 'dockerHub', usernameVariable: 'dockerHub', passswordVariable: 'dockerHubPass')])
 {
     sh "docker login -u ${env.dockerHub} -p ${env.dockerHubPass}"
+    docker push node-app:v25.08
 }
 }
