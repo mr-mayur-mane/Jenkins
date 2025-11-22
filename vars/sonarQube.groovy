3389c6f28edf8998db0953e7a9364f6664d63fe5
mr-mayu-mane <mayurmane2577@gmail.com>
2025-11-22 20:39:37 +0530

    Add the shared lib for the sonar scan

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -2,6 +2,23 @@ def call(Map config = [:]){
     if (!config.projectKey){
         error(Missing SonarQube Project)
     }
-    
-    #sonarqube api
+    def scannerHome = tool 'SonarScanner'
+    withSonarQubeEnv("sonarqube"){
+        withCredentails([string(credentialsId: 'sonar-token', variable: 'ONAR_TOKEN')]){
+            sh """
+                ${scannerHome}/bin/sonar-scanner \
+                  -Dsonar.projectKey=${config.projectKey} \
+                  -Dsonar.sources=${config.sources ?: '.'} \
+                  -Dsonar.host.url=${env.SONAR_HOST_URL} \
+                  -Dsonar.login=$SONAR_TOKEN
+            """
+        }
+    }
+    // Quality gate check
+    timeout(time: 2, unit: 'MINUTES') {
+        def qg = waitForQualityGate()
+        if (qg.status != 'OK') {
+            error "Quality Gate Failed: ${qg.status}"
+        }
+    }
 }
